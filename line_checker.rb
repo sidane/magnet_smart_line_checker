fdaa35a33dfd66403aba0829ed49e5119dc4b226
Niall Mullally <niall@4l.ie>
2010-05-29 14:05:14 +0100

    Added process action and basic flash message support

--- line_checker.rb
+++ line_checker.rb
@@ -2,6 +2,26 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 
+enable :sessions
+
 get "/" do
+  @error = request.cookies["error"]
   erb :index
+end
+
+post "/process" do
+  if params["phone_numbers"].empty?
+    session[:flash] = "Please enter at least one phone number"
+    redirect "/"
+  end
+end
+
+helpers do
+  def flash_message
+    unless session[:flash].nil?
+      value = session[:flash].dup
+      session[:flash] = nil
+      '<p class="flash">' + value + '</p>'
+    end
+  end
 end
