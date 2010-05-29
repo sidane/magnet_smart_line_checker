d796013cb08583e5882aea8cbca8869fc35d319d
Niall Mullally <niall@4l.ie>
2010-05-29 15:35:31 +0100

    Updating process action to render results and adding layout for views

--- line_checker.rb
+++ line_checker.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require File.dirname(__FILE__) + "/lib/line_check"
 
 enable :sessions
 
@@ -10,10 +11,9 @@ get "/" do
 end
 
 post "/process" do
-  if params["phone_numbers"].empty?
-    session[:flash] = "Please enter at least one phone number"
-    redirect "/"
-  end
+  validate_phone_number_params
+  @results = process_phone_numbers
+  erb :result
 end
 
 helpers do
@@ -24,4 +24,33 @@ helpers do
       '<p class="flash">' + value + '</p>'
     end
   end
+  
+  def render_results
+    unless @results.nil?
+      @results.inject([]) do |array, result|
+        array << "<h3>#{result[:phone_number]}</h3>"
+        array << "<p>"
+        array << "<strong>Magnet:</strong> #{result[:magnet_result]}</p>"
+      end.join("\n")
+    end
+  end
 end
+
+  private
+
+  def validate_phone_number_params
+    if params["phone_numbers"].empty?
+      session[:flash] = "Please enter at least one phone number"
+      redirect "/"
+    end
+  end
+
+  def process_phone_numbers
+    params["phone_numbers"].split(" ").inject([]) do |array, phone_number|
+      hash = {}
+      hash[:phone_number] = phone_number
+      hash[:magnet_result] = MagnetLineCheck.new(phone_number).process
+      array << hash
+    end
+  end
+  
